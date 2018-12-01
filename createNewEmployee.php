2a341977bdd93bb4d0b69ee85deceab78252dd65
lydia <lydia@mc-wifi-10-215-99-67.wireless.pitt.edu>
2018-12-01 13:55:56 -0500

    createNewEmployee done

--- createNewEmployee.php
+++ createNewEmployee.php
@@ -34,31 +34,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <title>create new employee</title>
     </head>
     <body>
-        <form method="post" action="createNewEmployee.php" autocomplete = "off"> 
-            <h2> Enter new employee information</h2>
-            username: <input type="text" name="username"> <br>
-            password: <input type="text" name="password"><br>
-            first name: <input type="text" name="firstName"> <br>
-            last name: <input type="text" name="lastName"><br>
-            address: <input type="text" name="address"><br>
-            city: <input type="text" name="city"><br>
-            state: <input type="text" name="state"><br>
-            zipcode:  <input type="number" name="zipcode"><br>
-            email: <input type="text" name="email"><br>
-            <br>
-            Store Location<br><select name="storeID">
-            <option value="1">Philadelphia</option>
-            <option value="2">Pittsburgh</option>
-            <option value="3">Boston</option>
-            <option value="4">New York</option>
-      
-            </select><br><br>
-            jobTitle<br> <input type="radio" name="jobTitle" value="employee" checked> employee
-            <input type="radio" name="jobTitle" value="manager"> manager 
-            <input type="radio" name="jobTitle" value="administrator"> administrator<br>
-            salary:<input type="number" name="salary">
-            <input type="submit" name="submit" value="Submit">
-        </form>
+        <div id="create-employee">
+            <form method="post" action="createNewEmployee.php" autocomplete = "off"> 
+                <h2> Enter new employee information</h2>
+                username: <input type="text" name="username"> <br>
+                password: <input type="text" name="password"><br>
+                first name: <input type="text" name="firstName"> <br>
+                last name: <input type="text" name="lastName"><br>
+                address: <input type="text" name="address"><br>
+                city: <input type="text" name="city"><br>
+                state: <input type="text" name="state"><br>
+                zipcode:  <input type="number" name="zipcode"><br>
+                email: <input type="text" name="email"><br>
+                Store Location: <select name="storeID">
+                <option value="1">Philadelphia</option>
+                <option value="2">Pittsburgh</option>
+                <option value="3">Boston</option>
+                <option value="4">New York</option>
+          
+                </select><br><br>
+                jobTitle: <input type="radio" name="jobTitle" value="employee" checked> employee
+                <input type="radio" name="jobTitle" value="manager"> manager 
+                <input type="radio" name="jobTitle" value="administrator"> administrator<br>
+                salary:<input type="number" name="salary">
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        </div>
         <?php
 
         echo $firstName;
